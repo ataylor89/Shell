82aa94d79ab055fe197d4645420e98860559c565
Andrew Taylor <rolyata47@gmail.com>
2023-07-01 07:54:45 -0500

    Organizing code

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -8,11 +8,11 @@ Shell::Shell() {}
 
 string Shell::exec(string cmd)
 {
-    string output;
-    char buffer[128];
     if (cmd.find_first_not_of(' ') == string::npos)
         return "";
     cmd += " 2>&1";
+    string output;
+    char buffer[128];
     FILE* pipe = popen(cmd.c_str(), "r");
     while (fgets(buffer, 128, pipe))
         output += buffer;
