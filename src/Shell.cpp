caa3741fa6a8878f49c289326166ded6f59c9a70
Andrew Taylor <rolyata47@gmail.com>
2023-06-30 20:38:59 -0500

    Formatting

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -7,19 +7,19 @@
 Shell::Shell() 
 : output_file("/tmp/shell.txt") 
 {
-    
+
 }
 
 string Shell::exec(string cmd)
 {
-        if (cmd.find_first_not_of(' ') == string::npos)
-            return "";
-        cmd += " &> " + output_file;
-        system(cmd.c_str());
-        ostringstream ss;
-        ss << ifstream(output_file).rdbuf();
-        string output = ss.str();
-        if (!output.empty() && output.back() != '\n')
-            output += "\n";
-        return output;
+    if (cmd.find_first_not_of(' ') == string::npos)
+        return "";
+    cmd += " &> " + output_file;
+    system(cmd.c_str());
+    ostringstream ss;
+    ss << ifstream(output_file).rdbuf();
+    string output = ss.str();
+    if (!output.empty() && output.back() != '\n')
+        output += "\n";
+    return output;
 }
