a98c24397f1f359d8093782d392c7d528d17d193
Andrew Taylor <rolyata47@gmail.com>
2023-07-02 10:38:14 -0500

    Improving the split algorithm

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -37,7 +37,7 @@ vector<string> StringUtils::split(const string& Str, const string& Delim, int li
     str = strdup(Str.c_str());
     delim = Delim.c_str();
 
-    if (limit > 1 && (token = strtok(str, delim)))
+    if ((token = strtok(str, delim)))
     {
         vec.push_back(token);
         limit--;
